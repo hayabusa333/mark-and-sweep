e633189690d5db31d1a730bd4982a4fdaa891bb1
root <root@www6095ue.sakura.ne.jp>
2012-11-06 23:10:07 +0900

    Start. I create mark-and-sweep

---
+++ gc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef struct OBJECT
+{
+    char mark;
+    struct Object *next;
+    void *p;
+    int size;
+}Object;
+
+typedef struct ROOT
+{
+    Object *p;
+    struct Root *next;
+}Root;
+
+typedef struct HEAPS
+{
+    char area[128];
+    char used_flag[128];
+}Heps;
+
+void main()
+{
+    Object test;
+
+    printf("%d\n", sizeof(test));
+
+    printf("OK\n");
+}
+
+
